661aff5cd91ad6dddcca7f95de0b06053f6206b9
okayama <kenmin.okayama@gmail.com>
2010-12-01 21:50:02 +0900

    First commit

---
+++ addons/MTCheck.pack/lib/MT/Check/L10N/en_us.pm
@@ -0,0 +1,9 @@
+package MT::Check::L10N::en_us;
+
+use strict;
+
+use base 'MT::Check::L10N';
+use vars qw( %Lexicon );
+%Lexicon = ();
+
+1;
---
+++ addons/MTCheck.pack/lib/MT/Check/L10N/ja.pm
@@ -0,0 +1,11 @@
+package MT::Check::L10N::ja;
+
+use strict;
+use base 'MT::Check::L10N::en_us';
+use vars qw( %Lexicon );
+
+%Lexicon = (
+	'This module is required in Twitter' => 'Twitter の API を利用するために必要となります。',
+);
+
+1;
